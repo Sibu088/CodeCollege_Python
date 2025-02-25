853cff2f823555140a524ef457f859d85e5d931d
Sibu088 <mavismadira6@gmail.com>
2025-02-25 17:28:24 +0200

    chapter 3

---
+++ chapter 3/bicycles.py
@@ -0,0 +1,23 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[1])
+
+cars =['lamborghini','ferrari','porche','Aston Martin']
+print(cars[0].title())
+
+cars =['lamborghini','ferrari','porche','Aston Martin']
+message = f"My first car was a {cars[-1].title()}."
+
+print(message)
+
+
+my_list =['Cadee','Ulric','Lesedi','Aiden','Mieke','Ethan','Lindo','Ronny','Asanda','Tom','Courtney']
+
+my_list.insert(4,'Prince king')
+print(my_list)
+
+del my_list[1]
+number_5 = my_list.pop()
+
+alphabetical_list =["b","a","c"]
+print(sorted(alphabetical_list))
+print(alphabetical_list)
---
+++ chapter 3/guest_list.py
@@ -0,0 +1,10 @@
+guests=['guido van rossum','jack turner','iynn hill']
+
+name = guests[2].title()
+print(f"{name},please come to dinner.")
+
+name = guests[1].title()
+print(f"{name},please come to dinner.")
+
+name = guests[2].title()
+print(f"{name},please come to dinner.")
